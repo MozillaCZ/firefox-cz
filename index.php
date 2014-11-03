6cdda37cb4301c6c8396e62b5a207814397505fc
Pavel Cvrcek <jasnapaka@jasnapaka.com>
2014-11-03 23:30:17 +0100

    Obsah webu.

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+    header('Content-Type: text/html; charset=UTF-8');
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-language" content="cs" />
+<title>Mozilla Firefox</title>
+<link rel="stylesheet" href="firefox-cz.css" type="text/css" media="screen" />
+<link rel="shortcut icon" href="favicon.png" type="image/png" />
+<meta name="author" content="HTML code: Adam Hauner; e-mail: aha@pinknet.cz" />
+<meta name="robots" content="index,follow" />
+</head>
+<body id="www-firefox-cz">
+<div class="page">
+<h1><img src="firefox-ozw.png" alt="Mozilla Firefox &ndash; objevte znovu web" /></h1>
+<hr />
+<dl>
+	<dt><a href="http://www.mozilla.org/cs/firefox/">Mozilla Firefox</a></dt>
+	<dd>Oficiální stránky rychlého a&nbsp;bezpečného webového prohlížeče,
+	o&nbsp;kterém všichni nadšeně mluví.</dd>
+
+	<dt><a href="https://support.mozilla.com/cs/">Podpora uživatelů</a></dt>
+	<dd>Oficiální stránky podpory uživatelů Mozilla Firefoxu. Web obsahuje
+	řadu tipů a&nbsp;návodů, které vám mohou pomoci.</dd>
+
+	<dt><a href="http://forum.mozilla.cz/viewforum.php?f=3">Diskusní fórum</a></dt>
+	<dd>Webové fórum české komunity Mozilla Firefoxu pro podporu uživatelů. Máte-li
+	 problém, je to místo určené právě vám.</dd>
+
+	<dt><a href="https://addons.mozilla.org/cs/firefox/">Doplňky pro Firefox</a></dt>
+	<dd>Rozšiřte si	Firefox podle svých představ pomocí nepřeberného
+	množství doplňků. Vytvořte si svůj Firefox!</dd>
+
+	<dt><a href="http://www.mozilla.cz">Česká komunita uživatelů</a></dt>
+	<dd>Stránky české komunity Mozilla, kde můžete nalézt informace o&nbsp;aktuálním
+	dění, a&nbsp;kde se též můžete do projektu zapojit.</dd>
+</dl>
+<hr />
+<div class="foot">
+	<a href="http://www.mozilla.org/" title="Domovská stránka Mozilly" hreflang="en">Mozilla.org</a>
+	&middot;
+	Hostuje: <a href="http://www.flexibee.eu/">FlexiBee Systems s.r.o.</a>
+</div>
+<div class="webstats">
+	<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+		try {
+			var pageTracker = _gat._getTracker("UA-10350415-2");
+			pageTracker._trackPageview();
+		} catch(err) {}
+	</script>
+</div>
+</div>
+</body>
+</html>
